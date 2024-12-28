df1da7695388d8663f934ad0461872878793f717
Praise Ekpenisi <praiseekpenisi@outlook.com>
2024-12-28 14:14:39 +0100

    forgot to update the latest version

--- PostgreSQL/Ecommerce/Scripts/create databse object.sql
+++ PostgreSQL/Ecommerce/Scripts/create databse object.sql
@@ -1,11 +1,10 @@
-CREATE schema altschool;
 set search_path to altschool;
 
 CREATE TABLE customers(
 	customer_id SERIAL PRIMARY KEY,
 	name CHAR(50) NOT NULL,
 	email VARCHAR(250) UNIQUE,
-	phone_number BIGINT UNIQUE,
+	phone_number VARCHAR(20) UNIQUE,
 	address VARCHAR(250)
 );
 
@@ -14,12 +13,12 @@ CREATE TABLE orders(
 	customer_id INT,
 	order_date DATE NOT NULL,
 	total_amount DECIMAL(15,2) NOT NULL,
-	FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
+	FOREIGN KEY(customer_id) REFERENCES customers(customer_id) ON DELETE CASCADE
 );
 
 CREATE TABLE products(
 	products_id SERIAL PRIMARY KEY,
-	products_name VARCHAR(100) NOT NULL,
+	products_name VARCHAR(200) NOT NULL,
 	category VARCHAR(100) NOT NULL,
 	price DECIMAL(10,2) NOT NULL,
 	stock_quantity INT NOT NULL
@@ -31,6 +30,6 @@ CREATE TABLE order_items(
 	products_id INT,
 	quantity INT NOT NULL,
 	price DECIMAL(10,2) NOT NULL,
-	FOREIGN KEY(order_id) REFERENCES orders(order_id),
-	FOREIGN KEY(products_id) REFERENCES products(products_id)
+	FOREIGN KEY(order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
+	FOREIGN KEY(products_id) REFERENCES products(products_id) ON DELETE CASCADE
 );
